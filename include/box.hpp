b3b954e241437ffe393c95493203e7b39cdb823e
Kishan Patel <bishan.batel@protonmail.com>
2024-04-25 00:44:45 -0700

    Documentation and const correctness

--- include/box.hpp
+++ include/box.hpp
@@ -6,6 +6,7 @@
 #include <utility>
 
 #include "debug.hpp"
+#include "ref.hpp"
 
 namespace crab::box {
   template<typename T>
@@ -26,7 +27,12 @@ namespace crab::box {
 };
 
 /**
- * Owned Pointer (RAII) to an instance of T on the heap.
+ * @brief Owned Pointer (RAII) to an instance of T on the heap.
+ *
+ * This is a replacement for std::unique_ptr, with two key differences:
+ *
+ * - Prevents Interior Mutability, being passed a const Box<T>& means dealing with a const T&
+ * - A Box<T> has the invariant of always being non null, if it is then you messed up with move semantics.
  */
 template<typename T>
   requires(not std::is_const_v<T>)
@@ -64,22 +70,44 @@ private:
   }
 
 public:
+  /**
+   * @brief Wraps pointer with RAII
+   *
+   * This function has no way of knowing if the pointer passed is actually on
+   * the heap or if something else has ownership, therefor 'unchecked' and it is
+   * the responsibility of the caller to make sure.
+   */
   static Box wrap_unchecked(const MutPtr ref)
     requires IS_SINGLE {
     return Box(ref);
   };
 
+  /**
+   * @brief Wraps pointer to an array with RAII
+   *
+   * This function has no way of knowing if the pointer passed is actually on
+   * the heap or if something else has ownership, therefor 'unchecked' and it is
+   * the responsibility of the caller to make sure.
+   */
   static Box wrap_unchecked(const MutPtr ref, const SizeType length)
     requires IS_ARRAY {
     return Box(ref, length);
   };
 
+  /**
+   * @brief Reliquenshes ownership & opts out of RAII, giving you the raw
+   * pointer to manage yourself. (equivalent of std::unique_ptr<T>::release
+   */
   static MutPtr unwrap(Box box) requires IS_SINGLE {
     const auto ptr = box.raw_ptr();
     box.obj = nullptr;
     return ptr;
   }
 
+  /**
+   * @brief Reliquenshes ownership & opts out of RAII, giving you the raw
+   * pointer to manage yourself. (equivalent of std::unique_ptr<T>::release
+   */
   static std::pair<MutPtr, SizeType> unwrap(Box box) requires IS_ARRAY {
     return std::make_pair(
       std::exchange(box.obj, nullptr),
@@ -115,11 +143,21 @@ public:
   ~Box() { drop(); }
 
   // ReSharper disable once CppNonExplicitConversionOperator
-  operator Contained &() { return *raw_ptr(); } // NOLINT(*-explicit-constructor)
+  __always_inline operator Contained &() { return *raw_ptr(); } // NOLINT(*-explicit-constructor)
 
   // ReSharper disable once CppNonExplicitConversionOperator
-  operator const Contained &() const { return *raw_ptr(); } // NOLINT(*-explicit-constructor)
+  __always_inline operator const Contained &() const { return *raw_ptr(); } // NOLINT(*-explicit-constructor)
 
+  // ReSharper disable once CppNonExplicitConversionOperator
+  __always_inline operator Ref<Contained>() const {
+    return crab::ref::from_ptr_unchecked(raw_ptr());
+  }
+
+  __always_inline operator RefMut<Contained>() {
+    return crab::ref::from_ptr_unchecked(raw_ptr());
+  }
+
+  // NOLINT(*-explicit-constructor)
   void operator=(const Box &) = delete;
 
   void operator=(Box &&rhs) noexcept
@@ -135,13 +173,13 @@ public:
     size = std::exchange(rhs.size, crab::box::helper<T>::DEFAULT_SIZE);
   }
 
-  [[nodiscard]] MutPtr operator->() { return as_ptr(); }
+  [[nodiscard]] __always_inline MutPtr operator->() { return as_ptr(); }
 
-  [[nodiscard]] ConstPtr operator->() const { return as_ptr(); }
+  [[nodiscard]] __always_inline ConstPtr operator->() const { return as_ptr(); }
 
-  [[nodiscard]] Contained &operator*() { return *as_ptr(); }
+  [[nodiscard]] __always_inline Contained &operator*() { return *as_ptr(); }
 
-  const Contained &operator*() const { return *as_ptr(); }
+  __always_inline const Contained &operator*() const { return *as_ptr(); }
 
   friend std::ostream &operator<<(std::ostream &os, const Box &rhs) {
     return os << *rhs;
@@ -166,7 +204,12 @@ public:
   [[nodiscard]] ConstPtr as_ptr() const { return raw_ptr(); }
 
 private:
-  MutPtr raw_ptr() const {
+  MutPtr raw_ptr() {
+    debug_assert(obj != nullptr, "Invalid Use of Moved Box<T>.");
+    return obj;
+  }
+
+  ConstPtr raw_ptr() const {
     debug_assert(obj != nullptr, "Invalid Use of Moved Box<T>.");
     return obj;
   }
@@ -174,7 +217,7 @@ private:
 
 namespace crab {
   /**
-   * Makes a new instance of type T on the heap with given args
+   * @brief Makes a new instance of type T on the heap with given args
    */
   template<typename T, typename... Args>
     requires std::is_constructible_v<T, Args...>
@@ -183,7 +226,7 @@ namespace crab {
   }
 
   /**
-   * Creates a new array on the heap of 'count' size w/ default constructor for
+   * @brief Creates a new array on the heap of 'count' size w/ default constructor for
    * each element
    */
   template<typename T>
@@ -193,7 +236,7 @@ namespace crab {
   }
 
   /**
-   * Creats a new array on the heap and copies 'from' to each element
+   * @brief Creates a new array on the heap and copies 'from' to each element
    */
   template<typename T>
   static Box<T[]> make_boxxed_array(const usize count, const T &from)
@@ -205,14 +248,22 @@ namespace crab {
     return box;
   }
 
+  /**
+   * @brief Reliquenshes ownership & opts out of RAII, giving you the raw
+   * pointer to manage yourself. (equivalent of std::unique_ptr<T>::release
+   */
   template<typename T> requires Box<T>::IS_SINGLE
   static typename Box<T>::MutPtr release(Box<T> box) {
     return Box<T>::unwrap(std::move(box));
   }
 
+  /**
+   * @brief Reliquenshes ownership & opts out of RAII, giving you the raw
+   * pointer to manage yourself. (equivalent of std::unique_ptr<T>::release
+   */
   template<typename T> requires Box<T>::IS_ARRAY
   static std::pair<typename Box<T>::MutPtr, typename Box<T>::SizeType>
-  unwrap(Box<T> box) {
+  release(Box<T> box) {
     return Box<T>::unwrap(std::move(box));
   }
 }

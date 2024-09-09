62d6fa5a9414499c99653c84b93f8190ff6c3357
Kishan S Patel <bishan.batel@protonmail.com>
2024-09-08 20:40:05 -0700

    Added flake.nix from shell.nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+    forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+      pkgs = import nixpkgs { 
+          inherit system; 
+      };
+    });
+  in {
+    devShells = forEachSupportedSystem ({ pkgs }: {
+      default = pkgs.mkShell.override {}
+        {
+          shellHook = /* bash */ '' '';
+
+          packages = with pkgs; [
+              ninja
+              cmake
+              python3
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            clang
+            clang-tools
+          ];
+
+        };
+    });
+  };
+}

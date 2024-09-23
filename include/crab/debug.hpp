d94b5e1e37d73a32b647153e6b2e7aefedf02508
Kishan S Patel <bishan.batel@protonmail.com>
2024-09-23 03:22:34 -0700

    Replaced macros with std::source_location

--- include/crab/debug.hpp
+++ include/crab/debug.hpp
@@ -4,6 +4,7 @@
 
 #pragma once
 #include <format>
+#include <source_location>
 #include "../preamble.hpp"
 
 namespace crab::debug {
@@ -11,29 +12,38 @@ namespace crab::debug {
     std::string fmt;
 
   public:
-    explicit AssertionFailedError(
-        StringView function, StringView source, StringView assertion_text, usize line, StringView msg) :
-        fmt{std::format("Failed Assertion in:\n {}:{} in {} \n'{}'\n{}", source, line, function, assertion_text, msg)} {
-    }
+    explicit AssertionFailedError(std::source_location source_location, StringView assertion_text, StringView msg) :
+        /* fmt{std::format("Failed Assertion in:\n {}:{} in {} \n'{}'\n{}", source, line, function, assertion_text,
+           msg)} { */
+        fmt{std::format(
+            "Failed Assertion in:\n {}:{}: in {} \n'{}'\n{}",
+            source_location.file_name(),
+            source_location.line(),
+            source_location.column(),
+            source_location.function_name(),
+            assertion_text,
+            msg)} {}
 
     ~AssertionFailedError() override = default;
 
     [[nodiscard]] auto what() const noexcept -> const char * final { return fmt.c_str(); }
   };
 
-  inline unit
-  dbg_assert(StringView function, StringView source, StringView assertion_line, usize line, StringView msg) {
-    throw AssertionFailedError{function, source, assertion_line, line, msg};
+  inline unit dbg_assert(std::source_location source_location, StringView assertion_text, StringView msg) {
+    throw AssertionFailedError{source_location, assertion_text, msg};
   }
 } // namespace crab::debug
   //
 #if DEBUG
-  #define debug_assert(condition, message)                                                                             \
+  #define debug_assert_transparent(condition, message, source_location)                                                \
     if (!static_cast<bool>(condition)) do {                                                                            \
-        crab::debug::dbg_assert(__FUNCTION__, __FILE__, #condition, __LINE__, (message));                              \
+        crab::debug::dbg_assert(source_location, #condition, (message));                                               \
     } while (false)
+
+  #define debug_assert(condition, message) debug_assert_transparent(condition, message, std::source_location::current())
 #else
 
+  #define debug_assert_transparent(...)
   #define debug_assert(...)
 
 #endif

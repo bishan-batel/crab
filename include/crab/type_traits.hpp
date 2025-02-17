0c685cf67b941e684caf3950b0cefa665f1eacb8
Kishan S Patel <bishan.batel@protonmail.com>
2025-02-16 23:49:51 -0800

    Format

--- include/crab/type_traits.hpp
+++ include/crab/type_traits.hpp
@@ -3,14 +3,21 @@
 //
 
 #pragma once
+#include <concepts>
 #include <type_traits>
+#include <utility>
 
 namespace crab {
   namespace ref {
-    template<typename T>
-    concept is_valid_type = not std::is_const_v<T> and not std::is_reference_v<T> and not std::is_volatile_v<T>;
+    template<typename T> concept is_valid_type = not std::is_const_v<T>
+                                             and not std::is_reference_v<T>
+                                             and not std::is_volatile_v<T>;
   }
 
-  template<typename T>
-  concept is_complete_type = requires { sizeof(T); };
+  template<typename T> concept is_complete_type = requires { sizeof(T); };
+
+  template<std::move_constructible T>
+  constexpr auto identity =
+    [](T&& val) -> T&& { return std::forward<T&&>(val); };
+
 } // namespace crab

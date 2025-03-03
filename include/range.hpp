ba2924dcb98a91a26d7c3867e6ace1889dd7cc62
Kishan S Patel <bishan.batel@protonmail.com>
2025-03-02 17:27:59 -0800

    Documentation

--- include/range.hpp
+++ include/range.hpp
@@ -13,6 +13,9 @@ class Range final {
 
 public:
 
+  /**
+   * Iterator type for Range
+   */
   struct Iterator {
     using iterator_category = std::output_iterator_tag;
     using difference_type = std::ptrdiff_t;
@@ -52,24 +55,45 @@ public:
     T pos;
   };
 
+  /**
+   * Constructs a range from min to max, this will panic if max > min
+   */
   Range(T min, T max): min(min), max(max) {
     debug_assert(min <= max, "Invalid Range, max cannot be greater than min");
   }
 
+  /**
+   * Returns the lower bound of this range
+   */
   [[nodiscard]] constexpr auto upper_bound() const -> T { return max; }
 
+  /**
+   * Returns the lower bound of this range
+   */
   [[nodiscard]] constexpr auto lower_bound() const -> T { return min; }
 
+  /**
+   * Begin iterator position.
+   */
   [[nodiscard]] constexpr auto begin() const -> Iterator {
     return Iterator(min);
   }
 
+  /**
+   * End iterator position.
+   */
   [[nodiscard]] constexpr auto end() const -> Iterator { return Iterator(max); }
 
+  /**
+   * Returns the length of this range
+   */
   [[nodiscard]] constexpr auto size() const -> usize {
     return static_cast<usize>(max - min);
   }
 
+  /**
+   * @brief Checks if the given value is within this range
+   */
   [[nodiscard]] constexpr auto contains(const T value) const -> bool {
     return min <= value and value < max;
   }

3eec16de4828c6e6d1ebe73cf950c22969f94aff
Kishan S Patel <bishan.batel@protonmail.com>
2026-01-11 15:25:06 -0800

    Removed options for  fmtlib

--- include/crab/fmt.hpp
+++ include/crab/fmt.hpp
@@ -7,74 +7,17 @@
 #include <concepts>
 #include <sstream>
 
-// Enum for what format library crab will use
-#define CRAB_FMT_USAGE_STD           0
-#define CRAB_FMT_USAGE_FMTLIB        1
-#define CRAB_FMT_USAGE_COMPATABILITY 2
-
-// Determining support for std::format
-#if CRAB_HAS_INCLUDE("format")
-#define CRAB_SUPPORTS_STD_FORMAT true
-#else
-#define CRAB_SUPPORTS_STD_FORMAT false
-#endif
-
-// Determine support for fmt::format
-#if CRAB_HAS_INCLUDE("fmt/format.h")
-#define CRAB_SUPPORTS_FMTLIB true
-#else
-#define CRAB_SUPPORTS_FMTLIB false
-#endif
-
-// Automatically select what formatter to use
-#ifndef CRAB_FMT_USAGE
-
-// rely on std::format, else use compatability
-#if CRAB_SUPPORTS_STD_FORMAT
-#define CRAB_FMT_USAGE CRAB_FMT_USAGE_STD
-#else
-#define CRAB_FMT_USAGE CRAB_FMT_USAGE_COMPATABILITY
-#endif
-
-#endif
-
-// Picking what formatter to use
-
-#if CRAB_FMT_USAGE == CRAB_FMT_USAGE_STD
-
-#if CRAB_SUPPORTS_STD_FORMAT
-#include <format>
-#else
-#error                                                                                                                 \
-  "Compiler does not support the use of std::format, consider using CRAB_FMT_USAGE=1 to use fmtlib or compatability mode"
-#endif
-
-#elif CRAB_FMT_USAGE == CRAB_FMT_USAGE_FMTLIB
-
-#if CRAB_SUPPORTS_FMTLIB
-
 #if CRAB_CLANG_VERSION
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-literal-operator"
 #endif
 
+#include <fmt/core.h>
+#include <fmt/base.h>
 #include <fmt/format.h>
 
 #if CRAB_CLANG_VERSION
 #pragma clang diagnostic pop
-#endif
-
-#else
-#error "CRAB_USE_FMT_LIB is true but fmtlib is not available."
-#endif
-
-#elif CRAB_FMT_USAGE == CRAB_FMT_USAGE_COMPATABILITY
-
-#else
-
-#error                                                                                                                 \
-  "Macro CRAB_FMT_USAGE_COMPATABILITY set to an invalid number, must either be 0 (std::format), 1 (fmt::format), or 2 (compatability)"
-
 #endif
 
 namespace crab {
@@ -99,8 +42,6 @@ namespace crab {
     }(obj);
   }
 
-#if CRAB_FMT_USAGE == CRAB_FMT_USAGE_FMTLIB
-
   template<typename T>
   [[nodiscard]] constexpr auto to_string(T&& obj) -> String {
     if constexpr (requires(const T& obj) { fmt::to_string(std::forward<T>(obj)); }) {
@@ -109,34 +50,6 @@ namespace crab {
       return builtin_to_string<T>(std::forward<T>(obj));
     }
   }
-#else
-  template<typename T>
-  [[nodiscard]] constexpr auto to_string(T&& obj) -> String {
-    return builtin_to_string(std::forward<T>(obj));
-  }
-#endif
-
-#if CRAB_FMT_USAGE == CRAB_FMT_USAGE_STD
-
-  using std::format;
-
-#elif CRAB_FMT_USAGE == CRAB_FMT_USAGE_FMTLIB
 
   using fmt::format;
-
-#else
-  namespace helper::fmt {}
-
-  template<typename... Args>
-  [[nodiscard]] auto format(auto&& fmt, Args&&... args) -> String {
-    std::stringstream stream{};
-    stream << "FORMAT_STRING(\"";
-    stream << fmt;
-    stream << "\") % (";
-    (..., (stream << ", " << to_string(args)));
-    stream << ')';
-
-    return std::move(stream).str();
-  }
-#endif
 }
--- run_tests.nu
+++ run_tests.nu
@@ -39,13 +39,11 @@ def "main windows" [] {
 	cmake "-Wno-dev" "-B" $build_dir "-DCRAB_TESTS=ON" $"-DCMAKE_BUILD_TYPE=($build_type)" $"-DCMAKE_C_COMPILER=($compiler.c)" $"-DCMAKE_CXX_COMPILER=($compiler.cpp)" "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 }
 
-def cmake_test [build_type: string, compiler: record, fmtstd: bool, fmtlib: bool] {
-	let fmtstd = if $fmtstd { "ON" } else { "OFF" };
-	let fmtlib = if $fmtlib { "ON" } else { "OFF" };
-	let builddir = $"build/_($compiler.name)_($build_type)_fmtstd-($fmtstd)_fmtlib-($fmtlib)"
+def cmake_test [build_type: string, compiler: record] {
+	let builddir = $"build/_($compiler.name)_($build_type)"
 
 	print "> Setting up cmake"
-	cmake -Wno-dev "-B" $builddir "-DCRAB_TESTS=ON" $"-DCMAKE_BUILD_TYPE=($build_type)" $"-DCMAKE_C_COMPILER=($compiler.c)" $"-DCMAKE_CXX_COMPILER=($compiler.cpp)" "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" "-DCPM_USE_LOCAL_PACKAGES=OFF" $"-DCRAB_USE_FMT=($fmtlib)" $"-DCRAB_USE_STD_FORMAT=($fmtstd)" -GNinja
+	cmake "-B" $builddir "-DCRAB_TESTS=ON" $"-DCMAKE_BUILD_TYPE=($build_type)" $"-DCMAKE_C_COMPILER=($compiler.c)" $"-DCMAKE_CXX_COMPILER=($compiler.cpp)" "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" "-DCPM_USE_LOCAL_PACKAGES=OFF" -GNinja
 	# $"-G($generator)" 
 
 	print "> Compiling"
@@ -72,29 +70,22 @@ def main [] {
 		for debug in [true, false] {
 			let build_type = if $debug { "Debug" } else { "Release" };
 
-			for fmtstd in [false, true] {
-				for fmtlib in [false, true] {
+			let name = $"($compiler.name) on ($build_type)";
 
-					if $fmtstd == $fmtlib { continue; };
-
-					let name = $"($compiler.name) on ($build_type), fmtstd=($fmtstd), fmtlib=($fmtlib)";
-
-					$tests = $tests | append {
-						name: $name,
-						test: {||
-							try {  
-								cmake_test $build_type $compiler $fmtstd $fmtlib
-							} catch { 
-								error make --unspanned {
-									msg: $"Failed tests for ($name)",
-								}
-								exit
-							}
+			$tests = $tests | append {
+				name: $name,
+				test: {||
+					try {  
+						cmake_test $build_type $compiler
+					} catch { 
+						error make --unspanned {
+							msg: $"Failed tests for ($name)",
 						}
-					};
-
+						exit
+					}
 				}
-			}
+			};
+
 		}
 	}
 
@@ -105,8 +96,8 @@ def main [] {
 		print $"Passed Compiler ($x.name)"
 	};
 
-	$tests | par-each $run_test
-	# $tests | each $run_test
+	# $tests | par-each $run_test
+	$tests | each $run_test
 
 	print "" "--" ""
 

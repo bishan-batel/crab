48bc832178933137c6da9357d147ac8b6ab4f9bc
Kishan S Patel <bishan.batel@protonmail.com>
2026-01-24 22:51:10 -0800

    Allow mem::copy to copy from const T*

--- include/crab/mem/copy.hpp
+++ include/crab/mem/copy.hpp
@@ -10,12 +10,15 @@
 namespace crab::mem {
 
   template<ty::non_const T>
-  auto copy(T* source, T* destination, const usize count) -> void {
-    crab_check(source != nullptr && destination != nullptr, "mem::copy cannot copy from or to nullptr");
+  auto copy(const T* source, T* destination, const usize count) -> void {
+    // null checks
+    crab_dbg_check(source != nullptr && destination != nullptr, "mem::copy cannot copy from or to nullptr");
 
-    // check for alignment
-    crab_check(static_cast<uptr>(source) % alignof(T) == 0, "mem::copy must copy from a correctly aligned pointer");
-    crab_check(
+    // source msut be aligned
+    crab_dbg_check(static_cast<uptr>(source) % alignof(T) == 0, "mem::copy must copy from a correctly aligned pointer");
+
+    // dest must be aligned
+    crab_dbg_check(
       static_cast<uptr>(destination) % alignof(T) == 0,
       "mem::copy must copy into a correctly aligned pointer"
     );
@@ -25,8 +28,8 @@ namespace crab::mem {
     if constexpr (SIZE == 1) {
       memmove(static_cast<void*>(destination), static_cast<void*>(source), count);
     } else {
-      // check for integer overflow
-      crab_check(
+      // check for integer overflow when multiplying
+      crab_dbg_check(
         SIZE == 0 or count > std::numeric_limits<usize>::max() / SIZE,
         "mem::copy integeroverflow converting count -> byte_count"
       );
@@ -38,22 +41,26 @@ namespace crab::mem {
   }
 
   template<ty::non_const T>
-  auto copy_nonoverlapping(T* source, T* destination, const usize count) -> void {
-    crab_check(source != nullptr && destination != nullptr, "mem::copy_nonoverlapping cannot copy from or to nullptr");
+  auto copy_nonoverlapping(const T* source, T* destination, const usize count) -> void {
+    crab_dbg_check(
+      source != nullptr && destination != nullptr,
+      "mem::copy_nonoverlapping cannot copy from or to nullptr"
+    );
 
     // check for alignment
-    crab_check(
+    crab_dbg_check(
       static_cast<uptr>(source) % alignof(T) == 0,
       "mem::copy_nonoverlapping must copy from a correctly aligned pointer"
     );
 
-    crab_check(
+    crab_dbg_check(
       static_cast<uptr>(destination) % alignof(T) == 0,
       "mem::copy_nonoverlapping must copy into a correctly aligned pointer"
     );
 
     static constexpr usize SIZE{mem::size_of<T>()};
 
+    // double check that the ranges dont overlap
     {
       uptr source_begin{static_cast<uptr>(&source[0])};
       uptr source_end{static_cast<uptr>(&source[count - 1])};
@@ -61,7 +68,7 @@ namespace crab::mem {
       uptr dest_begin{static_cast<uptr>(&destination[0])};
       uptr dest_end{static_cast<uptr>(&destination[count - 1])};
 
-      crab_check(
+      crab_dbg_check(
         not(source_begin <= dest_end and dest_begin >= source_end),
         "mem::copy_nonoverlapping must operate on two non overlapping spans"
       );
@@ -73,7 +80,7 @@ namespace crab::mem {
       memcpy(static_cast<void*>(destination), static_cast<void*>(source), count);
     } else {
       // check for integer overflow
-      crab_check(
+      crab_dbg_check(
         SIZE == 0 or count > std::numeric_limits<usize>::max() / SIZE,
         "mem::copy_nonoverlapping integeroverflow converting count -> byte_count"
       );

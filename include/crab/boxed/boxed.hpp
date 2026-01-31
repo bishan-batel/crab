d17d9117182c43e4b7c0d8e5195f94507692b108
Kishan S Patel <bishan.batel@protonmail.com>
2026-01-30 21:31:07 -0800

    Box group

--- include/crab/boxed/boxed.hpp
+++ include/crab/boxed/boxed.hpp
@@ -1,6 +1,11 @@
+/// @file crab/boxed/boxed.hpp
+
 #pragma once
 
-/**
- * Namespace pertaining to Box<T>, a smart pointer with single ownership.
- */
+/// @defgroup boxed Box (Owned Heap Memory)
+///
+
+/// @namespace crab::boxed
+/// @ingroup boxed
+/// Namespace pertaining to Box<T>, a smart pointer with single ownership.
 namespace crab::boxed {}

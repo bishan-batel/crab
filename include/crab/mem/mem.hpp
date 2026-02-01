21c2c1668506797142a29389bb4c43e57eb9dc9e
Kishan S Patel <bishan.batel@protonmail.com>
2026-01-31 22:53:30 -0800

    Module level documentation for mem

--- include/crab/mem/mem.hpp
+++ include/crab/mem/mem.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
-/**
- * @namespace crab::mem
- * This namespace relates to raw memory / type memory management functions for crab. This includes things like
- * std::address_of, crab::move, crab::replace, as well as size_of, copy, swap, etc
- */
+/// @defgroup mem Memory
+/// Memory related functions in crab both perterning to object lifetime as well as manipulation of raw bytes.
+
+/// @namespace crab::mem
+/// @ingroup mem
+/// This namespace relates to raw memory / type memory management functions for crab. This includes things like
+/// std::address_of, crab::move, crab::replace, as well as size_of, copy, swap, etc
 namespace crab::mem {}

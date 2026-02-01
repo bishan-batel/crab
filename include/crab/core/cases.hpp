f10efb539d1cd9e2c3ce28301871add23e585d34
Kishan S Patel <bishan.batel@protonmail.com>
2026-02-01 12:46:16 -0800

    Cases documentation

--- include/crab/core/cases.hpp
+++ include/crab/core/cases.hpp
@@ -1,11 +1,27 @@
+/// @file crab/core/cases.hpp
+
 #pragma once
 
 namespace crab {
 
-  /**
-   * @brief Utility class for easily creating a Visitor instance when using
-   * std::visit and alike
-   */
+  /// @ingroup core
+  /// Utility class for easily creating a Visitor instance when using
+  /// std::visit and alike. The basis of this class is turning many functors into one for use with the 'Visitor'
+  /// pattern.
+  ///
+  ///
+  /// # Examples
+  /// ```cpp
+  /// std::variant<i32, String> v = String{"hello"};
+  ///
+  /// std::visit(
+  ///   crab::cases{
+  ///     [](const i32 &i) { fmt::println("Value is i32: {}", i); },
+  ///     [](const String &s) { fmt::println("Value is string: {}", s); }
+  ///   },
+  ///   v
+  /// );
+  /// ```
   template<typename... Functions>
   struct cases final : Functions... {
     using Functions::operator()...;

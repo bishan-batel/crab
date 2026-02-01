eefdedc8946dd0fe8d0662d80f10c31cc24bcd7a
Kishan S Patel <bishan.batel@protonmail.com>
2026-02-01 02:34:51 -0800

    crab::ops Documentation

--- include/crab/ops/Mul.hpp
+++ include/crab/ops/Mul.hpp
@@ -1,24 +1,27 @@
+/// @file crab/ops/Mul.hpp
+
 #pragma once
 
 #include <utility>
 
+/// @addtogroup ops
+/// @{
+
 namespace crab::ops {
 
-  /**
-   * Constraint for the existence of a operator* overload between a value of a type between another
-   *
-   * @tparam T
-   */
+  /// Constraint for the existence of a operator* overload between a value of a type between another
+  ///
+  /// @tparam T
   template<typename T, typename U = T>
   concept Mul = requires(T x, U y) { x * y; };
 
-  /**
-   * Evaluated type for the result of multiplying two values (of type T, U) together
-   *
-   * @tparam T
-   */
+  /// Evaluated type for the result of multiplying two values (of type T, U) together
+  ///
+  /// @tparam T
   template<typename T, typename U = T>
   requires Mul<T, U>
   using MulOutput = decltype(std::declval<T>() * std::declval<U>());
 
 }
+
+/// }@
--- include/crab/ops/ops.hpp
+++ include/crab/ops/ops.hpp
@@ -1,7 +1,12 @@
+/// @file crab/ops/ops.hpp
+
 #pragma once
 
-/**
- * @namespace crab::ops
- * @brief The crab::ops namespace contains type traits & helpers related to operators and operator overloads.
- */
+/// @defgroup ops Operations
+/// The operations library in crab are typically concepts / constraints relating to
+/// the use of operator(s) between types
+
+/// @namespace crab::ops
+/// @ingroup ops
+/// The crab::ops namespace contains type traits & helpers related to operators and operator overloads.
 namespace crab::ops {}

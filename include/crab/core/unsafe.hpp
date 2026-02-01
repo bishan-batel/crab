13b1aa097e9c713a485d55b3ad66def404176575
Kishan S Patel <bishan.batel@protonmail.com>
2026-01-31 22:34:00 -0800

    Added unsafe marker to core

--- include/crab/core/unsafe.hpp
+++ include/crab/core/unsafe.hpp
@@ -1,4 +1,4 @@
-/// @file
+/// @file crab/core/unsafe.hpp
 
 #include <array>
 #include <bit>
@@ -8,11 +8,13 @@ namespace crab {
 
   /// Unsafe marker type, this type is used
   /// This type should appear as an unamed parameter
+  /// @ingroup core
   struct unsafe_fn final {
     unsafe_fn() = delete;
   };
 
   /// Constant  meant to be passed to functions marked as 'unsafe' with the tag 'unsafe_t'.
+  /// @ingroup core
   static constexpr unsafe_fn unsafe{
     std::bit_cast<unsafe_fn>(std::array<u8, sizeof(unsafe_fn)>{}),
   };

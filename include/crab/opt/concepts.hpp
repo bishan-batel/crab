39d1f2cea490a6ddb4516333f49fea15e6e4f959
Kishan S Patel <bishan.batel@protonmail.com>
2026-02-03 10:01:47 -0800

    Added internal tag to impl blocks in is_option_type

--- include/crab/opt/concepts.hpp
+++ include/crab/opt/concepts.hpp
@@ -11,12 +11,14 @@ namespace crab::opt {
     /// Type predicate helper for if the given type T is an option type, false
     /// for most types
     /// @ingroup opt
+    /// @internal
     template<typename>
     struct is_option_type final : ty::false_type {};
 
     /// Type predicate helper for if the given type is an option type, true
     /// only if the given type is of the form Option<T> for some T
     /// @ingroup opt
+    /// @internal
     template<typename T>
     struct is_option_type<opt::Option<T>> final : ty::true_type {};
 
